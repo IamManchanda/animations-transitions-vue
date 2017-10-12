ec0cb08502c34251b53dabbdd91c49581034f0b1
harry <harmanmanchanda182@gmail.com>
2017-10-12 20:22:25 +0530

    Create JavaScript Animations!

--- src/App.js
+++ src/App.js
@@ -2,7 +2,56 @@ export default {
   data () {
     return {
       show: true,
-      calloutAnimation: 'bounce'
+      calloutAnimation: 'bounce',
+      elementWidth: 60
     };
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log('beforeEnter');
+      this.elementWidth = 60;
+      el.style.width = this.elementWidth + '%';
+    },
+    enter(el, done) {
+      console.log('enter');
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = (this.elementWidth + round * 2) + '%';
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    },
+    afterEnter(el) {
+      console.log('afterEnter');
+    },
+    enterCancelled(el) {
+      console.log('enterCancelled');
+    },
+    beforeLeave(el) {
+      console.log('beforeLeave');
+      this.elementWidth = 100;
+      el.style.width = this.elementWidth + '%';
+    },
+    leave(el, done) {
+      console.log('leave');
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = (this.elementWidth - round * 2.5) + '%';
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    },
+    afterLeave(el) {
+      console.log('afterLeave');
+    },
+    leaveCancelled(el) {
+      console.log('leaveCancelled');
+    }
   }
 };
--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,15 @@
         <p>JavaScript Version</p>
         <hr> 
         <button class="button primary" @click="show = !show">Toggle Info</button>
-        <transition mode="out-in" name="slide" type="animation" appear>
-          <div class="callout primary" key="primary" v-if="show">
+        <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" @enter-cancelled="enterCancelled" @before-leave="beforeLeave" @leave="leave" @after-leave="afterLeave" @leave-cancelled="leaveCancelled" mode="out-in" :css="false" appear>
+          <div class="callout primary" key="primary" style="width: 60%;" v-if="show">
             <h5>This is the Primary Information</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat earum aliquid, incidunt nulla harum voluptates sequi, repellat explicabo, veritatis ipsam aliquam minima! Corporis debitis placeat saepe asperiores, voluptatibus omnis. In.</p>
             <button class="close-button" aria-label="Dismiss alert" type="button" @click="show = false">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div class="callout alert" key="alert" v-else>
+          <div class="callout alert" key="alert" style="width: 100%;" v-else>
             <h5>This is an Alert Information</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat earum aliquid, incidunt nulla harum voluptates sequi, repellat explicabo, veritatis ipsam aliquam minima! Corporis debitis placeat saepe asperiores, voluptatibus omnis. In.</p>
             <button class="close-button" aria-label="Dismiss alert" type="button" @click="show = true">
@@ -29,4 +29,4 @@
 
 <script src="./App.js"></script>
 
-<style lang="scss" src="./App.scss"></style>
+<style lang="scss"></style>
