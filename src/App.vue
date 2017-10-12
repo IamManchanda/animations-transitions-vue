977105e37e928f4ea50b96e88a5811b999468923
harry <harmanmanchanda182@gmail.com>
2017-10-12 17:28:35 +0530

    Create a new aimation and transition project!

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="grid-container margin-top-2">
+    <div class="grid-x grid-margin-x">
+      <div class="cell medium-8 medium-offset-2">
+        <h1>Animations &amp; Transitions</h1>
+        <hr> 
+        <!-- This select dropdown is not working right now, will fix it soon. -->
+        <!-- <select v-model="calloutAnimation"> 
+          <option value="bounce">Bounce</option>
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+          <option value="zoom">Zoom</option>
+        </select>
+        <h3> {{ calloutAnimation }} </h3>
+        <hr /> -->
+        <button class="button primary" @click="show = !show">Toggle Info</button>
+        <transition mode="out-in" enter-class="" enter-active-class="animated bounceInLeft" leave-class="" leave-active-class="animated bounceOutRight" appear>
+          <div class="callout primary" key="primary" v-if="show">
+            <h5>This is the Primary Information</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat earum aliquid, incidunt nulla harum voluptates sequi, repellat explicabo, veritatis ipsam aliquam minima! Corporis debitis placeat saepe asperiores, voluptatibus omnis. In.</p>
+            <button class="close-button" aria-label="Dismiss alert" type="button" @click="show = false">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="callout alert" key="alert" v-else>
+            <h5>This is an Alert Information</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat earum aliquid, incidunt nulla harum voluptates sequi, repellat explicabo, veritatis ipsam aliquam minima! Corporis debitis placeat saepe asperiores, voluptatibus omnis. In.</p>
+            <button class="close-button" aria-label="Dismiss alert" type="button" @click="show = true">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        show: true,
+        calloutAnimation: 'bounce'
+      };
+    }
+  };
+</script>
+
+<style lang="scss" src="./App.scss"></style>
